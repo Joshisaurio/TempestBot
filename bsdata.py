fe505f5d8ba055de5340e091f8c6c822d816c19f
Joshisaurio <joshmanjarrez13@gmail.com>
2025-07-08 20:46:30 -0600

    Unfinished user saving

--- bsdata.py
+++ bsdata.py
@@ -3,6 +3,8 @@ import os
 import urllib.parse
 from dotenv import load_dotenv
 
+import data
+
 load_dotenv()
 
 brawl_key = os.getenv('BRAWL_KEY')
@@ -21,5 +23,8 @@ def get_player(tag):
     r = requests.get(url, headers=headers)
     print(r.json())
     return r.json()
+
+def get_user(discord_id:str):
+    return get_player(data[discord_id])
 
 get_player("PLOV929GC")
--- info.py
+++ info.py
@@ -1,14 +1,17 @@
+tempest_clubs = ["#PL0V929GC", "#JLQ888Q0", "#80L8YUY02"]
+club_names = {"PL0V929GC":"Main", "#JLQ888Q0":"Apprentice", "#80L8YUY02":"Elite"}
+
 commands = [
     {# Hello command
     "name":"hello",
     "description":"Hello, world! (This is a test)",
     "options":[]
     },
-    {# Crazycaps command
+    {# Profile command
     "name":"profile",
     "description":"Check a player's profile.",
     "options":[
-        { # Crazycaps - Parameter 1
+        { # Profile - Parameter 1
         "type": 3,
          "name": "player_id",
          "description": "ID of the player's profile",
